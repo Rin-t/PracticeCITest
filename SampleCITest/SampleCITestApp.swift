345038ebefdd806b198fef9f121605e162bf20b5
Rin-t <rin.t.4921@gmail.com>
2024-11-10 11:01:35 +0900

    Initial Commit

---
+++ SampleCITest/SampleCITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCITestApp.swift
+//  SampleCITest
+//
+//  Created by Rin on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleCITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleCITestTests/SampleCITestTests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleCITestTests.swift
+//  SampleCITestTests
+//
+//  Created by Rin on 2024/11/10.
+//
+
+import Testing
+
+struct SampleCITestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
